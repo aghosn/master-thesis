21d114e6a4a1ee201045be0bbfa0e9781791caf3
Adrien Ghosn <aghosn@Cain-2.local>
2016-03-06 19:11:17 -0500

    Cleaning up

--- MasterThesis/Code/insertOSRExit.h
+++ MasterThesis/Code/insertOSRExit.h
@@ -8,8 +8,8 @@ typedef std::vector<Instruction *> InstVector;
  * @param      optimized     The from function.
  * @param      toInstrument  The model for the continuation function.
  * @param      src           The instruction at which we insert the exit.
- * @param[in]  condition     The condition to fire the exit.
- * @param[in]  compensation	 Global compensation code to execute at OSR Entry.	
+ * @param      condition     The condition to fire the exit.
+ * @param  	   compensation	 Global compensation code to execute at OSR Entry.	
  *
  * @return     The optimized function and the continuation created.
  */
